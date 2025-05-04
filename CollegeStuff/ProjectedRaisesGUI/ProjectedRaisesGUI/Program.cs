0ff8c0e7f0e3640bb6476bb54cce512d3d90ee0d
mattmoorega <mattmoore6000@gmail.com>
2025-05-03 20:34:31 -0400

    Add files via upload

---
+++ CollegeStuff/ProjectedRaisesGUI/ProjectedRaisesGUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectedRaisesGUI
+{    static class ProjectedGUI
+    {
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SalaryCalculator());
+        }
+    }
+}
