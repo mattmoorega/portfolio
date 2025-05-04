856efd0c32de53aa5844d51c8496b470b8db21f7
mattmoorega <mattmoore6000@gmail.com>
2025-05-04 01:00:00 -0400

    Delete CollegeStuff directory

--- CollegeStuff/Java/PizzaStuff/src/DemoPizza.java
+++
@@ -1,9 +0,0 @@
-public class DemoPizza {
-    public static void main(String[] args) {
-        Pizza pizza1 = new Pizza("Cheese", 5);
-        DeliveryPizza pizza2 = new DeliveryPizza("Pepperoni", 7, "112 Boogey Ave.");
-
-        pizza1.printPizza();
-        pizza2.printPizza();
-    }
-}
