1859866119d963b8bbb7393d66dfb59e795588bb
mattmoorega <mattmoore6000@gmail.com>
2025-05-16 01:40:11 -0400

    Created on 01/29/2023

---
+++ College Stuff/Python (CIST 2742)/FiveStarPlace.py
@@ -0,0 +1,4 @@
+new_videos=int(input("Enter amount of new videos: "))
+oldies=int(input("Enter amount of old videos: "))
+total=((new_videos*3.00)+(oldies*2.00))
+print("The total cost is","$"+str(total))
