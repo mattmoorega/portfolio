63ccd96ea993db523a4b49317c7dab67a8ab90dc
mattmoorega <mattmoore6000@gmail.com>
2025-05-15 23:22:49 -0400

    Created on 10/17/2022

---
+++ College Stuff/C# (CIST 2341 & 2342)/Averages/Averages/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Averages
+{
+    static void Main(string[] args)
+    {
+        double sum = 0; // Variable for sum, which is required to calculate average.
+        double average; // Variable for average, no need to set a value yet
+
+        Console.Write("Enter any amount of numbers separated by a space > "); // Requests input from program user.
+        string userInput = Console.ReadLine(); // The string used to read the user's input.
+
+        string[] NumSet = userInput.Split(' '); // Makes a new array entry each time there's a space.
+
+        int NumAmt = NumSet.Length; // Determines how many numbers are in the array.
+
+        int count = 0;
+        while (count < NumAmt)
+        {
+            sum += Convert.ToDouble(NumSet[count]);
+            count++;
+        }
+
+        average = sum / NumAmt; // Calculates our average.
+        Console.WriteLine("The sum of the numbers that were input is " + sum);
+        Console.WriteLine("Which means the average is " + average);
+    }
+}
