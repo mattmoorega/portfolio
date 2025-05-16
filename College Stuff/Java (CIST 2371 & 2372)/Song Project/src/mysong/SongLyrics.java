cbbe179354b3e2ff2c092337d4b85a9dbae5d056
mattmoorega <mattmoore6000@gmail.com>
2025-05-16 00:49:35 -0400

    Created on 09/05/2022

---
+++ College Stuff/Java (CIST 2371 & 2372)/Song Project/src/mysong/SongLyrics.java
@@ -0,0 +1,11 @@
+package mysong;
+
+public class SongLyrics {
+	public static void main(String[] args)
+	{
+	System.out.println("You say we spend the day on");
+	System.out.println("The beach and we can just play");
+	System.out.println("I'm sure you really nice but");
+	System.out.println("I can't remember your name");
+}
+}
