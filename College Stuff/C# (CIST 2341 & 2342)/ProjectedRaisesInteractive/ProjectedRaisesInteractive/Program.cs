6373bd88081b854a8d2ae5609bc30a03e737f340
mattmoorega <mattmoore6000@gmail.com>
2025-05-15 23:54:24 -0400

    Created on 09/06/2022

---
+++ College Stuff/C# (CIST 2341 & 2342)/ProjectedRaisesInteractive/ProjectedRaisesInteractive/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class ProjectedRaisesInteractive
+{
+
+    static void Main()
+    {
+        const double raiseamount = 1.04;
+        Console.WriteLine("Please input your salary to determine what it will be after a 4 percent raise.");
+        double employeeSalary = double.Parse(Console.ReadLine());
+        Console.WriteLine("Your new salary will be: " + raiseamount * employeeSalary);
+    }
+
+}
